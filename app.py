b89f24a0a9867311ae1341afa3ade7e895f30c47
syipmong <syipmong5@gmail.com>
2023-12-29 08:13:00 +0100

    testing the db

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
 
 @app.route('/')
 def home():
-    return render_template('index.html', greeting='Hello, Flask!')
+    users = User.query.all()
+    return render_template('index.html', greeting='Hello, Flask!', users=users)
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    username = request.form['username']
+    new_user = User(username=username)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
